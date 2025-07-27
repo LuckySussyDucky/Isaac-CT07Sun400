56f82e5598eea110dfade34213d172b4270d9a2f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 08:17:07 +0000

    Jul 27, 2025, 8:17 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -9,7 +9,7 @@ function setup() {
   let b = 10;
   let sum = a + b;
   let product = a * b;
-  console.log()
+  console.log("The sum is, " + sum);
   // --- Exercise: Area of Triangle ---
   // write your codes here
 
