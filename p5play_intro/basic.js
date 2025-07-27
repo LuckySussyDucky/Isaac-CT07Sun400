f2cb7ad9f32b636217e4fc57e5ac1b2e83bf7560
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 08:53:00 +0000

    Jul 27, 2025, 8:53 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -63,6 +63,8 @@ function setup() {
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
 
+  
+
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 }
