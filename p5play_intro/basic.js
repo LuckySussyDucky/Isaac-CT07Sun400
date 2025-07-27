5cb36c7fed9a2833dcd1ffe64715d4f62c33dfc9
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 09:13:25 +0000

    Jul 27, 2025, 9:13 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -75,7 +75,7 @@ function setup() {
   let yOdd = yEven + 100
   while (num >= 1){
     console.log(num);
-    text(num, )
+    text(num, xOdd, yOdd);
     oddSum = oddSum + num;
     num = num - 2;
   }
