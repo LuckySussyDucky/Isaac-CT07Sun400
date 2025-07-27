1418927268371b6a90b235cf975e171511caea5f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 08:35:15 +0000

    Jul 27, 2025, 8:35 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -28,7 +28,7 @@ function setup() {
 
   let sum = 0;
   for (let i = 2; i <= 20; i+=2){
-    sum = sum + i
+    sum = sum + i;
   };
   console.log("The sum of the first 10 even numbers is " + sum)
   text("The sum of the first 10 even numbers is " + sum, 20, 60)
