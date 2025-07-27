1a891793575ee9cb262c446fae67440ffd166410
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 08:31:51 +0000

    Jul 27, 2025, 8:31 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -27,7 +27,7 @@ function setup() {
   // write your codes here
 
   for (let i = 2; i <= 20; i+=2){
-    console.log("Counting... " + i)
+    let
   }
 
   // --- Exercise: Age category classification ---
