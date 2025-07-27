e57f9103a8e18e9d502aea4d602baa7bf3648769
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-07-27 08:33:36 +0000

    Jul 27, 2025, 8:33 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -29,8 +29,8 @@ function setup() {
   let sum = 0;
   for (let i = 2; i <= 20; i+=2){
     sum = sum + i
-    console.log("The sum of the first 10 even numbers is " + sum)
   };
+  console.log("The sum of the first 10 even numbers is " + sum)
 
   // --- Exercise: Age category classification ---
   // write your codes here
