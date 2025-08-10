7d1ea4dbcc4b453d205bf0460e4660109b21e273
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-10 09:15:33 +0000

    Aug 10, 2025, 9:15 AM

--- flappybird/playground/pg05b_pipeobstacles.js
+++ flappybird/playground/pg05b_pipeobstacles.js
@@ -38,7 +38,7 @@ function setup(){ //must have function
     bird.bounciness = 0.5;
     world.gravity.y = 10
 
-    birdRed
+    birdRed = new Sprite();
 
     floor = new Sprite();
     floor.x = 200;
