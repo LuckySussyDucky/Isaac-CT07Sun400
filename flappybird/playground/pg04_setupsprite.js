fcc40a7f7fb6e80898e5833404399391f1f202c1
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-10 07:38:47 +0000

    Aug 10, 2025, 7:38 AM

--- flappybird/playground/pg04_setupsprite.js
+++ flappybird/playground/pg04_setupsprite.js
@@ -6,7 +6,7 @@ function preload(){ //load the images before the game starts
     background = loadImage("assets/background-day.png");
     base = loadImage("assets/base.png");
     flapMidImg = loadImage("assets/yellowbird-midflap.png");
-
+}
 
 function setup(){ //must have function
     new Canvas(400, 600);
