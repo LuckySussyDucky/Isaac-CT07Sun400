535ed0f04a2c74562ef007ef6fdd85d4bbfe6a50
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-10 09:10:58 +0000

    Aug 10, 2025, 9:10 AM

--- flappybird/playground/pg05b_pipeobstacles.js
+++ flappybird/playground/pg05b_pipeobstacles.js
@@ -1,6 +1,6 @@
 let background, base;
 let bird, floor;
-let flapDownImgY, flapUpImgY, flapMidImgY, flapDownImgB, flapUpImgB, flapMidImgB, flapDownIm;
+let flapDownImgY, flapUpImgY, flapMidImgY, flapDownImgB, flapUpImgB, flapMidImgB, flapDownImgR, flapDown;
 let bird2;
 let pipeGroup; //declare the variable for the group
 let pipe; //used to preload the pipe image
