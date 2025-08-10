08f57421d16b79309276401f225592c7f60216be
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-10 08:16:19 +0000

    Aug 10, 2025, 8:16 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -7,7 +7,7 @@ function preload(){ //load the images before the game starts
     base = loadImage("assets/base.png");
     flapMidImg = loadImage("assets/yellowbird-midflap.png");
     flapDownImg = loadImage("assets/yellowbird-downflap.png");
-    flapUpImg = loadImage("assets/yellow")
+    flapUpImg = loadImage("assets/yellowbird")
 }
 
 function setup(){ //must have function
