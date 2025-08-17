07982756bd2f7f1e36b9e50959fdfa8a0f7ca8e5
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-17 08:15:18 +0000

    Aug 17, 2025, 8:15 AM

--- flappybird/playground/pg06_camscrolling.js
+++ flappybird/playground/pg06_camscrolling.js
@@ -1,102 +1,95 @@
-//two must have function 1)setup 2)draw
-let bird , floor;
-let bg, base, flapMidImg, flapDownImg, flapUpImg;
+let background, base;
+let bird, floor;
+let flapDownImg, flapUpImg, flapMidImg;
 let bird2;
-
-let pipeGroup;
-let pipe;
+let pipeGroup; //declare the variable for the group
+let pipe; //used to preload the pipe image
 let topPipe, bottomPipe;
 
-//load the images before game starts
-function preload(){
-  bg = loadImage('assets/background-day.png');
-  base = loadImage('assets/base.png');
-  flapMidImg = loadImage('assets/yellowbird-midflap.png');
-  flapUpImg = loadImage('assets/yellowbird-upflap.png');
-  flapDownImg = loadImage('assets/yellowbird-downflap.png');
-
-  pipe= loadImage("assets/pipe-green.png");
+function preload(){ //load the images before the game starts 
+    background = loadImage("assets/background-day.png");
+    base = loadImage("assets/base.png");
+    flapMidImg = loadImage("assets/yellowbird-midflap.png");
+    flapDownImg = loadImage("assets/yellowbird-downflap.png");
+    flapUpImg = loadImage("assets/yellowbird-upflap.png");
+    pipe = loadImage("assets/pipe-green.png")
 }
 
-function setup(){
-//to create a new Canvas
- new Canvas(400,600);
- bird = new Sprite();
- bird.x =  width/2;
- bird.y = 200;
- bird.width = 30;
- bird.height = 30;
- bird.img = flapMidImg;
-
- //bird physics
- bird.collider ='dynamic';
- bird.mass =2;
- bird.drag = 0.02;
- bird.bounciness = 0.5;
- world.gravity.y = 10;
- 
- floor = new Sprite();
- floor.x = 200;
- floor.y = height -20;
- floor.width = 400;
- floor.height = 125;
- floor.collider = 'static';
- floor.img = base;
-
- pipeGroup = new Group();
-
-}
-
-function draw(){
-  image(bg,0,0,width,height); // draw the bg
-
-  if(kb.presses('space') || mouse.presses('left')){
-    bird.vel.y = -5;
-    bird.sleeping = false;
-  }
-
-  // if(mouse.presses('left')){
-  //   bird2 = new Sprite(mouse.x,mouse.y,30,30,'dynamic');
-  //   bird2.img = flapMidImg
-  // }
-  // if(mouse.presses('right')){
-  //   bird2= new Sprite(mouse.x,mouse.y,30,30,'static');
-  //   bird2.img = flapMidImg
-  // }
-  
-  if(bird.vel.y <-1){
-    bird.img = flapUpImg;
-    bird.rotation = -30;
-  }else if(bird.vel.y>1){
-    bird.img = flapDownImg;
-    bird.rotation = 30;
-  }else{
+function setup(){ //must have function
+    new Canvas(400, 600);
+    bird = new Sprite();
+    bird.x = width / 2;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
     bird.img = flapMidImg;
-    bird.rotation = 0;
-  }
+    
+    //bird physics
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.5;
+    world.gravity.y = 10
 
-  if(frameCount===1){
-    spawnPipePair();
-  }
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static";
+    floor.img = base;
+
+    pipeGroup = new Group();
+}
+
+function draw(){ //must have function
+    image(background, 0, 0, width, height);
+    if(kb.presses("space") || mouse.presses("left")){
+        bird.vel.y = -5;
+        bird.sleeping = false;
+    }
+
+    if (bird.vel.y < -1){
+        bird.img = flapUpImg;
+        bird.rotation = -30;
+    }else if(bird.vel.y > 1){
+        bird.img = flapDownImg;
+        bird.rotation = 30;
+    }else{
+        bird.img = flapMidImg;
+        bird.rotation = 0;
+    }
+
+    if(frameCount === 1){
+        spawnPipePair();
+    }
+
+    // if(mouse.presses("left")){
+    //     bird2 = new Sprite(mouse.x, mouse.y, 30, 30, "dyanmic");
+    //     bird2.img = flapMidImg
+    // }
+    // if(mouse.presses("right")){
+    //     bird2 = new Sprite(mouse.x, mouse.y, 30, 30, "static");
+    //     bird2.img = flapMidImg
+    // }
 
 }
 
 function spawnPipePair(){
+    let gap = 50
+    let midY = height / 2
 
-    let gap= 50;
-    let midY = height / 2;
-
-
-    topPipe = new Sprite(400, midY-gap /2 -200, 52,320 , 'static')
-    topPipe.img=pipe;
-    topPipe.rotation=180;
-
-    bottomPipe=  new Sprite(400, midY+gap /2 +200, 52,320 , 'static') 
-    bottomPipe.img=pipe;
+    //create top pipe
+    topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, "static");
+    topPipe.img = pipe;
+    topPipe.rotation = 180;
 
     pipeGroup.add(topPipe);
+    pipeGroup.layer = 0;
+
+    //create bottom pipe
+    bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, "static");
+    bottomPipe.img = pipe;
+
     pipeGroup.add(bottomPipe);
-    pipeGroup.layer=0;
-
-
 }
