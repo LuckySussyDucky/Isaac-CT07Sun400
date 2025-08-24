020c8fd07ae73d0074aaede39e93d72b74280839
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-24 08:27:35 +0000

    Aug 24, 2025, 8:27 AM

--- flappybird/playground/pg07_.js
+++ flappybird/playground/pg07_.js
@@ -8,7 +8,7 @@ let topPipe, bottomPipe;
 let gameoverLabel, gameoverImg;
 let startScreenLabel, startScreenImg;
 let startGame = false;
-let score = 0, numberImages = [],scoreDigits
+let score = 0, numberImages = [], scoreDigits;
 
 function preload(){ //load the images before the game starts 
     background = loadImage("assets/background-day.png");
