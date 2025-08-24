9628c6875074e1ad471d578f2ef46c27dcea1c21
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-24 08:43:27 +0000

    Aug 24, 2025, 8:43 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -162,5 +162,5 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     scoreDigits.removeAll(); //clear old digits sprites
     let scoreStr = str(score);
     let totalWidth = scoreStr.length * digitWidth;
-    let startx = x - //state the starting x coordinates
+    let startx = x - totalWidth //state the starting x coordinates
 }
