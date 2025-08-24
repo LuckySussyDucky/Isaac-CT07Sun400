e58e2eb042cb15f8f08625caf9f7deb34f90baea
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-24 09:03:48 +0000

    Aug 24, 2025, 9:03 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -176,5 +176,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
 
 function moveGroup(group, targetX, spacing){
     let totalWidth = (group.length - 1) * spacing;
-    let startX = (targetX - )
+    let startX = (targetX - totalWidth / 2);
+    
 }
