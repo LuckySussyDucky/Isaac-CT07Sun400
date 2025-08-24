e05a8c298f081709b8a0ea28cd9493e4f31fa761
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-24 09:17:12 +0000

    Aug 24, 2025, 9:17 AM

--- flappybird/playground/revision.js
+++ flappybird/playground/revision.js
@@ -2,13 +2,14 @@ let ball;
 let floor;
 function setup(){
     new Canvas(800, 800);
+    background(250);
     world.gravity.y = 10;
 
     floor = new Sprite(0, 800, 800, 30, "static");
 }
 
 function draw(){
-    
+
     if (mouse.presses("left")){
         ball = new Sprite(mouseX,mouseY,30);
     }
