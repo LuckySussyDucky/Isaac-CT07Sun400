8163ad64e039b0fca64b10fbf36322e16127962f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-25 09:40:47 +0000

    Aug 25, 2025, 9:40 AM

--- flappybird/playground/testing.js
+++ flappybird/playground/testing.js
@@ -16,8 +16,8 @@ function preload(){ //load the images before the game starts
     background = loadImage("assets/background-day.png");
     base = loadImage("assets/base.png");
     flapMidImg = loadImage("assets/" + colour + "bird-midflap.png");
-    flapDownImg = loadImage("assets/yellowbird-downflap.png");
-    flapUpImg = loadImage("assets/yellowbird-upflap.png");
+    flapDownImg = loadImage("assets/" + colour + "bird-downflap.png");
+    flapUpImg = loadImage("assets/" + colour + "bird-upflap.png");
     pipe = loadImage("assets/pipe-green.png")
     gameoverImg = loadImage("assets/gameover.png")
     startScreenImg = loadImage("assets/message.png")
