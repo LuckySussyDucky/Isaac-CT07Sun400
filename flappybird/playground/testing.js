4099be03c4c3754086978a65d07ffa5d1537cdc6
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-08-31 09:02:18 +0000

    Aug 31, 2025, 9:02 AM

--- flappybird/playground/testing.js
+++ flappybird/playground/testing.js
@@ -28,8 +28,9 @@ function preload(){ //load the images before the game starts
       numberImages[i] = loadImage("assets/" + i + ".png")
     }
     for(let i = 0; i < 4; i++){
-      birdImages[i]
-      
+      if(i === 1){
+        
+      }
     }
 }
 
