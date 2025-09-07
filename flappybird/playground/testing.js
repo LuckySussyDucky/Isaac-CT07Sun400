aed4bef37269d4085b1786adfe7c4828d9075117
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-07 08:13:49 +0000

    Sep 7, 2025, 8:13 AM

--- flappybird/playground/testing.js
+++ flappybird/playground/testing.js
@@ -151,7 +151,7 @@ function draw(){ //must have function
         noLoop();
 }
 
-}
+}else if(colour ){}
     // if(mouse.presses("left")){
     //     bird2 = new Sprite(mouse.x, mouse.y, 30, 30, "dyanmic");
     //     bird2.img = flapMidImg
