1d007bf1bf0af21bd8b3ad93de1e52294289c85c
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-07 08:03:57 +0000

    Sep 7, 2025, 8:03 AM

--- flappybird/playground/testing.js
+++ flappybird/playground/testing.js
@@ -12,7 +12,7 @@ let score = 0;
 let numberImages = [];
 let scoreDigits;
 let birdImages = [];
-let colour = "";
+let colour = "yellow";
 let colourCode;
 let counter;
 let box;
@@ -87,6 +87,7 @@ function setup(){ //must have function
     scoreDigits.layer = 1000;
 
     box = new Sprite(200, 400, 75, 10, "static");
+    if(mouse.presses("left"))
     text("Colour: " + colour, 25, 40);
     text("Mouse: (" + mouseX + ", " + mouseY + ")", 25, 60);
 }
