c7db14e90c3164c9a4d597110c1fbf376152fc5f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-14 08:22:45 +0000

    Sep 14, 2025, 8:22 AM

--- flappybird/playground/practice.js
+++ flappybird/playground/practice.js
@@ -9,7 +9,8 @@ function setup(){
     square = new Sprite(800 / 2, 400 / 2, 25, 25, "static");
 
     circle = new Sprite(100, 350, 25, "dynamic");
-    
+    circle = 
+
     pipe = new Group();
 }
 function draw(){
