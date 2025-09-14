c698f4b34d1a9650ceacfdd14d27a510337c53ac
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-14 09:10:11 +0000

    Sep 14, 2025, 9:10 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -91,6 +91,7 @@ function draw(){ //must have function
       if(kb.presses("space") || mouse.presses("left")){
         bird.vel.y = -5;
         bird.sleeping = false;
+        
     }
 
     if (bird.vel.y < -1){
