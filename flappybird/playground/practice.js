db83f0870cbdbe731e353b88a27f3afcd9220960
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-14 08:54:22 +0000

    Sep 14, 2025, 8:54 AM

--- flappybird/playground/practice.js
+++ flappybird/playground/practice.js
@@ -16,7 +16,6 @@ function setup(){
     square = new Sprite(800 / 2, 400 / 2, 25, 25, "static");
 
     squareM = new Sprite(mouse.x, mouse.y, 25, 25, "static");
-    squareM.colour = "skyblue"
 
     circle = new Sprite(100, 350, 25, "dynamic");
     circle.bounciness = 0.5;
