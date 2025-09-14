cdd8835da5f948c76ac9a5be52c68a2a05bc488f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-14 08:05:38 +0000

    Sep 14, 2025, 8:05 AM

--- flappybird/playground/revision.js
+++ flappybird/playground/revision.js
@@ -4,7 +4,7 @@ function setup(){
     background(250);
     world.gravity.y = 10;
 
-    square = new Sprite(800 / 2, 800, 800);
+    square = new Sprite(800 / 2, 400 / 2, 800);
 }
 
 function draw(){
