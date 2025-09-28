f1dceabc785f68a33b9b96247997c9739ec874df
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-28 09:06:09 +0000

    Sep 28, 2025, 9:06 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -81,5 +81,6 @@ function splitFruit(x, y, fruitData){
     let left = new fruitHalves.Sprite(x - 10, y, 40, 40);
     left.img = fruitData.half1;
     left.vel.x = -3;
-    left.vel.y = random(-5, -10);
+    left.vel.y = random(-5, -2);
+    left
 }
