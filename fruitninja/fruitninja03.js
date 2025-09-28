e1bca6cef2cc6d05a26fea1bb2b1458ed601bdef
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-28 09:24:52 +0000

    Sep 28, 2025, 9:24 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -11,7 +11,8 @@ function preload() {
     }
     let watermelon = {
         whole : loadImage("assets/watermelonwhole.png"),
-        half1 : loadImage("assets/watermelonhalf.png")
+        half1 : loadImage("assets/watermelonhalf.png"),
+        half2 : loadImage("assets/watermelonhalfng")
     }
     fruitTypes = [peach, watermelon];
     dojoBG = loadImage("assets/dojobackground.png");
