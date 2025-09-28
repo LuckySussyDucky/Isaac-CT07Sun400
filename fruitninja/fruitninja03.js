f6d8a81d7e6410bb9a5b564c2875ccc083e682ec
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-28 08:47:37 +0000

    Sep 28, 2025, 8:47 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -58,7 +58,7 @@ function slicefruit(){
             continue;
         }
         let distance = dist(mouse.x, mouse.y, fruit.x, fruit.y);
-        if(distance < fruitD / 2 + 5){
+        if(distance < fruit.diameter / 2 + 5){
             fruit.sliced = true;
             fruit.remove;
             break;
