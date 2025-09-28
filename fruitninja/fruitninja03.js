9a5c729112e57d5ee0976c114774426fe4a7c628
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-28 08:41:39 +0000

    Sep 28, 2025, 8:41 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -13,12 +13,14 @@ function preload() {
     dojoBG = loadImage("assets/dojobackground.png");
 }
 
+
 function setup() {
     new Canvas(800, 600);
     background(250);
     world.gravity.y = 10;
     fruitGroup = new Group();
 }
+
 
 function draw() {
     clear();
