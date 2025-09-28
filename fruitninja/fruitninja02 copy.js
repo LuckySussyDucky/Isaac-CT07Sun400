85caf0bcaf0edfd11297a785bc7c371e61a8d32d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-09-28 08:33:57 +0000

    Sep 28, 2025, 8:33 AM

---
+++ fruitninja/fruitninja02 copy.js
@@ -0,0 +1,48 @@
+let dojoBG;
+let fruitGroup;
+let fruitTypes = [];
+let trail;
+function preload() {
+    let peach = {
+        whole : loadImage("assets/peachwhole.png"),
+    }
+    let watermelon = {
+        whole : loadImage("assets/watermelonwhole.png"),
+    }
+    fruitTypes = [peach, watermelon];
+    dojoBG = loadImage("assets/dojobackground.png");
+}
+
+function setup() {
+    new Canvas(800, 600);
+    background(250);
+    world.gravity.y = 10;
+    fruitGroup = new Group();
+}
+
+function draw() {
+    clear();
+    image(dojoBG, 0, 0, width, height);   
+
+    if(frameCount % 120 === 0){
+        spawnFruit();
+    }
+
+    if(mouse.pressing()){
+    trail = new Sprite(mouse.x, mouse.y, 7);
+    trail.collider = "none";
+    trail.color = "red";
+    trail.life = 10;
+    }
+}
+
+
+function spawnFruit(){
+    let fruitData = random(fruitTypes);
+    let randomX = random(300, 500);
+    let fruit = new fruitGroup.Sprite(randomX, height + 20, 40);
+    fruit.image = fruitData.whole;
+    fruit.vel.y = random(-10, -14);
+    fruit.vel.x = random(-2, 2);
+    fruit.friction = 0;
+}
