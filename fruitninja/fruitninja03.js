68ad9ab6c30963e307eca70c9cac04f8955a756f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-05 08:16:42 +0000

    Oct 5, 2025, 8:16 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -106,5 +106,6 @@ function displaySplash(){
         imageMode(CENTER);
         tint(255, map(splash.life, 0, 30, 0, 255)); //fade out splash
         image(splash.img, splash.x, splash.y, splash.size, splash.size);
+        
     }
 }
