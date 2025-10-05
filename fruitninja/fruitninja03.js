4072e9b98e4c51ba698f863b6d81809da87f5b5a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-05 08:12:22 +0000

    Oct 5, 2025, 8:12 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -102,6 +102,7 @@ function splitFruit(x, y, fruitData){
 function displaySplash(){
     for(let i = splashEffect.length; i >= 0; i--){
         let splash = splashEffect[i];
-        push
+        push();
+        imageMode
     }
 }
