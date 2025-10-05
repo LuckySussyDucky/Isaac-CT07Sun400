63cfd717197ff38cbcbbf31adefa9a674ad032bb
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-05 09:25:42 +0000

    Oct 5, 2025, 9:25 AM

--- fruitninja/fruitninja04.js
+++ fruitninja/fruitninja04.js
@@ -66,6 +66,7 @@ function draw(){
         textAlign(CENTER, CENTER);
         textFont("Fredoka One");
         text("Click to Start", width / 2, height / 2 + 50);
+        return;
     }
 
     stroke(158, 69, 69);
