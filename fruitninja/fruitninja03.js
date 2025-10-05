47f8a38333a7ab37fa8e810af567ffa2c4203e5a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-05 08:03:26 +0000

    Oct 5, 2025, 8:03 AM

--- fruitninja/fruitninja03.js
+++ fruitninja/fruitninja03.js
@@ -8,6 +8,7 @@ function preload() {
         whole : loadImage("assets/peachwhole.png"),
         half1 : loadImage("assets/peachhalf.png"),
         half2 : loadImage("assets/peachhalf2.png")
+        
     }
     let watermelon = {
         whole : loadImage("assets/watermelonwhole.png"),
@@ -16,8 +17,6 @@ function preload() {
     }
     fruitTypes = [peach, watermelon];
     dojoBG = loadImage("assets/dojobackground.png");
-    splashW = loadImage("assets/watermelonsplash.png");
-    splashP = loadImage("assets/peachsplash.png");
 }
 
 
