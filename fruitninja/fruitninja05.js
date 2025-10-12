9cec3d9e36783261919f831aba83bcd07b2d169a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-12 09:01:37 +0000

    Oct 12, 2025, 9:01 AM

--- fruitninja/fruitninja05.js
+++ fruitninja/fruitninja05.js
@@ -113,27 +113,6 @@ function draw(){
             fruit.remove();
         }
     }
-
-    stroke(158, 69, 69);
-    fill("255");
-    textSize(24);
-    textAlign(LEFT, TOP);
-    textFont("Fredoka One");
-    text("Missed: " + missed, 10, 30);
-
-    stroke(158, 69, 69);
-    fill("255");
-    textSize(24);
-    textAlign(LEFT, TOP);
-    textFont("Fredoka One");
-    text("Score: " + score, 10, 10);
-
-    gameTimer = floor((millis() - startTime) / 1000);
-        text("Time: " + (gameduration - gameTimer), 10, 50);
-        if(gameTimer >= gameduration){
-            gameState = "gameover";
-            return;
-    }
 }
 
 function spawnFruit(){
