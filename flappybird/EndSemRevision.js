f29b4eaef38cd862e2e63d01b617a4046ca30a55
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-19 08:33:08 +0000

    Oct 19, 2025, 8:33 AM

--- flappybird/EndSemRevision.js
+++ flappybird/EndSemRevision.js
@@ -23,7 +23,7 @@ function setup(){
 function draw(){
     image(background, 0, 0, width, height);
     
-    if(mouse.presses("left")){
+    if(mouse.presses("left") ){
         bird.y = bird.y - 50;
     }
 }
