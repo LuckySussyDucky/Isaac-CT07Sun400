045987c457979be207880038e317577dfddfa813
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-19 09:07:55 +0000

    Oct 19, 2025, 9:07 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -110,7 +110,7 @@ console.log(oddSum);
 
   age = 10;
   if (age <= 9){
-    console.log = "lower primary";
+    console.log("lower primary");
   }else if (age <= 12){
     console.log("upper primary");
   }else if (age <= 16){
