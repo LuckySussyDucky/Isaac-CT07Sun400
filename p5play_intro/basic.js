ff33460ee8382c33c1a05a504ac5ccce1305374c
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-19 09:01:10 +0000

    Oct 19, 2025, 9:01 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,4 +1,3 @@
-let height, base;
 function setup() {
   new Canvas(800, 600);
   background(250);
@@ -100,5 +99,7 @@ console.log(oddSum);
   for(let i = 0; i < 20; i += 2){
     console.log(i);
   }
+
+  
 
 }
