b7820f1874aa45b056d9c97882885440e8eb38a0
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-05 08:05:07 +0000

    Nov 5, 2025, 8:05 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -6,6 +6,6 @@ function setup(){
 }
 
 function draw(){
-    text("Guess the Word!", width / 2, 50);
+    text("Guess the Word!", width / 2, height / 2);
     text("Attempts: " + attempts, 250, 80);
 }
