d18beed82b282321552e92ab44de0db4e701dc1f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-05 08:14:06 +0000

    Nov 5, 2025, 8:14 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -8,7 +8,7 @@ function setup(){
     background(220);
 
     textBox = createInput();
-    textBox.position(width / 2 - textBox.size, height - 100);
+    textBox.position(width / 2 - textBox.width, height - 100);
     
 }
 
