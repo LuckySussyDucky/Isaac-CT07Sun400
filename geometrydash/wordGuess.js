2a0260c7124c30805dddd2fac0cc16602171687f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-05 08:09:00 +0000

    Nov 5, 2025, 8:09 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -1,6 +1,6 @@
 let attempts = 0;
 let words = [];
-let hint
+let hint;
 
 function setup(){
     new Canvas(600, 400);
@@ -12,5 +12,5 @@ function draw(){
     textAlign(CENTER, TOP);
     text("Guess the Word!", width / 2, 50);
     text("Attempts: " + attempts, width / 2, 100);
-    text
+    text("")
 }
