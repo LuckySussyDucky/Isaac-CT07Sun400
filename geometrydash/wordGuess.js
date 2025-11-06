f70f5431632375d4c97d90501eba50491be5eef5
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-06 10:08:07 +0000

    Nov 6, 2025, 10:08 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -56,8 +56,7 @@ function checkGuess() {
 
 function getCorrectLetters(guess, hiddenWord){
     let correctLetters = "";
-    for (let i = 0; i < word.length; i++) {
-
+    for (let i = 0; i < word.length; i++){
         if (word.includes(guess[i]) && !correctLetters.includes(guess[i].toUpperCase())) {
             correctLetters += guess[i].toUpperCase() + " ";
         }
