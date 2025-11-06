091cb566f1349d0e1796eaaca9a31fc1ed428a11
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-06 09:33:42 +0000

    Nov 6, 2025, 9:33 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -22,15 +22,16 @@ function setup(){
     hiddenWord = hiddenWord[0].toUpperCase() + "_ ".repeat(hiddenWord.length - 1);
     
     getCorrectLetters(textBox.value(), hiddenWord)
-}
 
-function draw(){
     textSize(16);
     textAlign(CENTER, TOP);
     text("Guess the Word!", width / 2, 50);
     text("Attempts: " + attempts, width / 2, 100);
     text("Hint: " + hiddenWord, width / 2, 150);
     text(display, width / 2, 200);
+}
+
+function draw(){
 
 
 }
