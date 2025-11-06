6f4e1577059f051abc12b701c3b3568326188358
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-11-06 09:48:44 +0000

    Nov 6, 2025, 9:48 AM

--- geometrydash/wordGuess.js
+++ geometrydash/wordGuess.js
@@ -48,7 +48,7 @@ function checkGuess() {
     let guess = textBox.value().toLowerCase();
     attempts++;
 
-    if (guess.toLowerCase() === hiddenWord.to) {
+    if (guess.toLowerCase() === hiddenWord.toLowerCase()) {
         message = "Correct! The word was '" + hiddenWord.toUpperCase() + "!";
     } 
     else if (guess.length != 5){
